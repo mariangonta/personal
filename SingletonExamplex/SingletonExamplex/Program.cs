3f3bf9d5b877873802140c09a5a88e653cffcee3
mariangonta <54546766+mariangonta@users.noreply.github.com>
2019-11-03 21:13:58 +0200

    -singleton exemplu

---
+++ SingletonExamplex/SingletonExamplex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonExamplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singletonExemplu1 = Singleton.CreareExemplu();
+            Console.WriteLine("exemplu 1 de singleton este {0}", singletonExemplu1.Id);
+            Singleton singletonExemplu2 = Singleton.CreareExemplu();
+            Console.WriteLine("Exemplu2 de singleton este {0}", singletonExemplu2.Id);
+
+            if (singletonExemplu1==singletonExemplu2)
+            {
+                Console.WriteLine("Se pare ca exeplu 1 si exemplu2 de singleton sunt identice !");
+            }
+            else { Console.WriteLine("Au fost create 2 exemple singleton ! "); };
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonExamplex/SingletonExamplex/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonExamplex
+{
+    class Singleton
+    {
+        private static  Singleton exemplu=new Singleton();
+        public int Id
+        {
+            get; set;
+        }
+        private Singleton()
+        {
+            Id =Id+ 1;
+        }
+        public static Singleton CreareExemplu()
+        {
+            
+            if (exemplu == null)
+            {
+                exemplu = new Singleton();
+            }
+            return exemplu;
+        }
+
+    }
+    
+}
